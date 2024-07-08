507e4be900014fa0463c4a600f7b90b958125873
Edgard Alas <edgardalasu@gmail.com>
2024-07-08 11:03:08 -0600

    chore: add Vite configuration and map HTML files

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { mapHTMLFiles } from './map-html-files';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: mapHTMLFiles(),
+    },
+  },
+});
